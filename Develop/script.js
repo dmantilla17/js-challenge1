2f1a063a250f8622b61b792179971ff0d4f7c8b7
dmantilla17 <derekaraujo11@gmail.com>
2023-03-05 19:50:01 -0500

    rearranging the code

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,52 @@
 // Assignment Code
-// how to randomize the password
-//Created Variables for the passsword to generate different options 
-var lowercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbersChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbolsChar = ['!', '@', '#', '$', '%', '&', '*', '(', ')'];
-
-//Created the generate button to select random numbers
 var generateBtn = document.querySelector("#generate");
+
 // Placing the function to create the passcode 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var passwordLength=prompt("How long would you like your password to be?");
+  //var passwordLength=prompt("How long would you like your password to be?");
   passwordText.value = password;
 }
-if (passwordLength <8 || passwordLength > 128) {
-  alert ("Must be atlest 8 characters and no more than 128 characters.");
-  return null; 
+function getPasswordLength(){
+  var length=parseInt(prompt("Choose a password between 8 and 128 characters."));
+//if statement
+if(( length< 8 || length > 128) || isNaN(length)) {
+  return getPasswordLength();
 }
+  return length;
+}
+function generatePassword(){
+  var lowercaseCharacters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbersCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbolsCharacters = ['!', '@', '#', '$', '%', '&', '*', '(', ')'];
+var chosenCharacters= ['',];
+var password=[''];
+
+var passwordLength= getPasswordLength();
+console.log(passwordLength);
+
+return password;
+}
+
+
+//do you want those numbers?
 //Setting up the questions that will be generated
-var symbolsChar= confirm("Would you like Symbols in your password?");
-var uppercaseChar=confirm("Would you like Uppercase Letters in your password?");
-var lowercaseChar=confirm("Would you like Lowercase Letters in your password?");
-var numbersChar=confirm("Would you like numbers in your password?");
-// Add event listener to generate button
-//Setting up the questions that will be generated
+if(confirm("Would you like to use numbers?")) chosenCharacters += numbersCharacters;
+if(confirm("Would you like to use lowercase characters?")) chosenCharacters += lowercaseCharacters;
+if(confirm("Would you like to use uppercase characters?")) chosenCharacters += uppercaseCharacters;
+if(confirm("Would you like to use special characters?")) chosenCharacters += symbolsCharacters;
+console.log(chosenCharacters);
+
+for (var i=0; i< passwordLength; i++ ){
+  var randomCharacter= chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)];
+  password += randomCharacter;
+}
+
+
+
+
 if ( 
   symbolsChar === false &&
   uppercaseChar === false &&
