fe58feb2fb3ca542c23e3a604bd7dc2fa76c47ca
dmantilla17 <derekaraujo11@gmail.com>
2023-03-11 14:22:03 -0500

    added comments and a readme

--- Develop/readme.md
+++ Develop/readme.md
@@ -1,9 +1,13 @@
 <text>
 
-
 ## NAME
+
 Generating a random password(s)
 
 ## Description
 
-created a password generator with javascript
+created a password generator with javascript to generate a password for the user by how many numbers they want in the password
+
+## Picture
+
+<img src="Assets/password.png"><img>
--- Develop/script.js
+++ Develop/script.js
@@ -1,92 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Placing the function to create the passcode 
+// Placing the function to create the passcode
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   //var passwordLength=prompt("How long would you like your password to be?");
   passwordText.value = password;
 }
-function getPasswordLength(){
-  var length=parseInt(prompt("Choose a password between 8 and 128 characters."));
-//if statement
-if(( length< 8 || length > 128) || isNaN(length)) {
-  return getPasswordLength();
-}
+//making qualifications for the password
+function getPasswordLength() {
+  var length = parseInt(
+    prompt("Choose a password between 8 and 128 characters.")
+  );
+  //if statement for the length of the password
+  if (length < 8 || length > 128 || isNaN(length)) {
+    return getPasswordLength();
+  }
+
   return length;
 }
-function generatePassword(){
-  var lowercaseCharacters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbersCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbolsCharacters = ['!', '@', '#', '$', '%', '&', '*', '(', ')'];
-var chosenCharacters= ['',];
-var password=[''];
+//adding characters to be able to generate the password
+function generatePassword() {
+  var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbersCharacters = "0123456789";
+  var symbolsCharacters = "!@#$%^&*()_+=";
+  var chosenCharacters = "";
+  var password = "";
 
-var passwordLength= getPasswordLength();
-console.log(passwordLength);
+  var passwordLength = getPasswordLength();
+  console.log(passwordLength);
 
-return password;
+  if (confirm("Would you like to use numbers?"))
+    chosenCharacters += numbersCharacters;
+  if (confirm("Would you like to use lowercase characters?"))
+    chosenCharacters += lowercaseCharacters;
+  if (confirm("Would you like to use uppercase characters?"))
+    chosenCharacters += uppercaseCharacters;
+  if (confirm("Would you like to use special characters?"))
+    chosenCharacters += symbolsCharacters;
+  console.log(chosenCharacters);
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomCharacter =
+      chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)];
+    password += randomCharacter;
+  }
+
+  return password;
 }
 
-
-//do you want those numbers?
-//Setting up the questions that will be generated
-if(confirm("Would you like to use numbers?")) chosenCharacters += numbersCharacters;
-if(confirm("Would you like to use lowercase characters?")) chosenCharacters += lowercaseCharacters;
-if(confirm("Would you like to use uppercase characters?")) chosenCharacters += uppercaseCharacters;
-if(confirm("Would you like to use special characters?")) chosenCharacters += symbolsCharacters;
-console.log(chosenCharacters);
-
-for (var i=0; i< passwordLength; i++ ){
-  var randomCharacter= chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)];
-  password += randomCharacter;
-}
-
-
-
-
-if ( 
-  symbolsChar === false &&
-  uppercaseChar === false &&
-  lowercaseChar === false &&
-  numbersChar === false &&
-     );
- {
-  alert("Must choose atleast one character type");
-  return null;
-}
-console.log(symbolsChar,uppercaseChar,lowercaseChar,numbersChar);
-
-//password library 
-var passwordLibrary = [];
-if (symbolsChar) {
-  passwordLibrary= passwordLibrary.concat(symbolsChar);
-}
-if (uppercaseChar) {
-  passwordLibrary = passwordLibrary.concat(uppercaseChar);
-}
-if (lowercaseChar) {
-  passwordLibrary = passwordLibrary.concat (uppercaseChar);
-}
-if (numbersChar) {
-  passwordLibrary = passwordLibrary.concat(numbersChar);
-}
-//loop
-var generatePassword="";
-for (var i=0;i < passwordLength; i++) {
-  var randomCharacter = passwordLibrary [Math.floor(Math.random() * passwordLibrary.length)];
-  generatePassword = generatePassword + randomCharacter;
-}
-
-return generatePassword;
-//password input//
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-passwordText.value= password
-
-}
+//password library
 generateBtn.addEventListener("click", writePassword);
-
